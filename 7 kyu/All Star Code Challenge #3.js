8db9cb340926b76d78d6047b84830f9bbec57384
Frediuk Olena <frediuk_olena@ukr.net>
2019-12-25 17:15:54 +0200

    All Star Code Challenge #3

---
+++ 7 kyu/All Star Code Challenge #3.js
@@ -0,0 +1,20 @@
+//All Star Code Challenge #3
+
+var removeVowels = function(str) {
+  let sound = 'aeiou';
+  let out =[];
+  str.split('');
+  for (let i = 0; i < str.length; i++) {
+    if (!sound.includes(str[i])) out.push(str[i]);
+  }
+  return out.join('');
+}
+
+// or
+
+var removeVowels = function(str) {
+    let sound = 'aeiou';
+    let out = '';
+    const s = str.split('').map(el => {if (!sound.includes(el)) out +=el});
+    return out;
+}
