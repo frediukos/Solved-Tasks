34e1cdb85eb51da2e04fbb1122af65eaa0d1bf4c
olenafrediuk <frediuk_olena@ukr.net>
2020-01-05 23:43:32 +0200

    Grasshopper tasks

---
+++ 8 kyu/Grasshopper - Combine strings.js
@@ -0,0 +1,18 @@
+// Grasshopper - Combine strings
+
+/*
+Combine strings function
+
+Create a function named combineNames(combine_names in python, ruby) that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+ */
+
+function combineNames(firstName, lastName) {
+    return firstName + ' ' + lastName;
+}
