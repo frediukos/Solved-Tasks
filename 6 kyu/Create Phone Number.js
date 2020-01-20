bb7d7b7f3b28042f99068f9ae5f012d71a4c2700
olenafrediuk <frediuk_olena@ukr.net>
2020-01-20 03:40:57 +0200

    Create Phone Number

--- 6 kyu/Create Phone Number.js
+++ 6 kyu/Create Phone Number.js
@@ -10,9 +10,8 @@ Don't forget the space after the closing parentheses!
  */
 
 function createPhoneNumber(numbers){
-    let str = "(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-"
+    return "(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-"
         + numbers[6] + numbers[7] + numbers[8] + numbers[9];
-    return str;
 }
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
