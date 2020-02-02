7d81c5d0394e9632e41e0f5ae214197c0595ab08
olenafrediuk <frediuk_olena@ukr.net>
2020-02-02 21:56:25 +0200

    Make the Deadfish swim

---
+++ 6 kyu/Make the Deadfish swim.js
@@ -0,0 +1,28 @@
+// Make the Deadfish swim
+
+/*
+Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+parse("iiisdoso") => [ 8, 64 ]
+ALGORITHMS  PARSING  STRINGS
+ */
+
+function parse( data ) {
+    let out = [];
+    let i = 0;
+    data.split('').map(el => {
+        if (el === 'i') { i++; }
+        else if (el === 'd') { i--; }
+        else if (el === 's') { i = i ** 2; }
+        else if (el === 'o') { out.push(i); }
+    });
+    return out;
+}
