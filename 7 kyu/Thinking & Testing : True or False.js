5cb2c6539a9c322ddc1c95b22510de63c7133976
olenafrediuk <frediuk_olena@ukr.net>
2020-02-03 02:03:33 +0200

    Thinking & Testing : True or False

---
+++ 7 kyu/Thinking & Testing : True or False.js
@@ -0,0 +1,15 @@
+// Thinking & Testing : True or False
+
+/*
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+ */
+
+function testit(n) {
+    return n.toString(2).replace(/0/g, '').length
+}
