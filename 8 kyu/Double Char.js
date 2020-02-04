2f2e14f04777d8d450a8062b67680d5638cfb37e
olenafrediuk <frediuk_olena@ukr.net>
2020-02-04 06:08:25 +0200

    Double Char

---
+++ 8 kyu/Double Char.js
@@ -0,0 +1,21 @@
+// Double Char
+
+/*
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+doubleChar("1234!_ ") ==> "11223344!!__  "
+Good Luck!
+FUNDAMENTALS LOOPS CONTROL FLOW  BASIC LANGUAGE FEATURES
+ */
+
+function doubleChar(str) {
+    newStr = [];
+    for (let i = 0; i < str.length; i++){
+        newStr += str[i].repeat(2);
+    }
+    return newStr;
+}
