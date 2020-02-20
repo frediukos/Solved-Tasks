8969f50a9b4b807c847fdba63c762147bb08e961
olenafrediuk <frediuk_olena@ukr.net>
2020-02-21 00:33:02 +0200

    Separating Strings

---
+++ 6 kyu/Separating Strings.js
@@ -0,0 +1,53 @@
+// Separating Strings
+
+/*
+Create a function that takes a string and separates it into an array of letters.
+
+The array will be formatted as so:
+
+[['J','L','L','M']
+,['u','i','i','a']
+,['s','v','f','n']
+,['t','e','e','']]
+The function should separate each word into individual letters, with the first word in the sentence having its letters in the 0th index of each 2nd dimension array, and so on.
+
+Shorter words will have an empty string in the place once the word has already been mapped out. (See the last element in the last part of the array.)
+
+Examples:
+
+sepStr("Just Live Life Man")
+// => [['J','L','L','M'],
+// => ['u','i','i','a'],
+// => ['s','v','f','n'],
+// => ['t','e','e','']]);
+
+sepStr("The Mitochondria is the powerhouse of the cell")
+// => [ [ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
+// => [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
+// => [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
+// => [ '', 'o', '', '', 'e', '', '', 'l' ],
+// => [ '', 'c', '', '', 'r', '', '', '' ],
+// => [ '', 'h', '', '', 'h', '', '', '' ],
+// => [ '', 'o', '', '', 'o', '', '', '' ],
+// => [ '', 'n', '', '', 'u', '', '', '' ],
+// => [ '', 'd', '', '', 's', '', '', '' ],
+// => [ '', 'r', '', '', 'e', '', '', '' ],
+// => [ '', 'i', '', '', '', '', '', '' ],
+// => [ '', 'a', '', '', '', '', '', '' ]]
+FUNDAMENTALS
+ */
+
+function sepStr(str) {
+  str = str.split(' ');
+  let out = []; let el = [];
+  let maxWord = Math.max(...str.map(el => el.length));
+  for (let i = 0; i < maxWord; i++) {
+    for (let j = 0; j < str.length; j++) {
+      if (str[j][i]) el.push(str[j][i])
+      else el.push('');
+    }
+    out.push(el);
+    el = [];
+  }
+  return out;
+}
