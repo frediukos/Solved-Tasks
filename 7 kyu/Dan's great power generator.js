06c229db2f8429ded57d35c30c4c11272bfa9c51
olenafrediuk <frediuk_olena@ukr.net>
2020-02-24 14:48:56 +0200

    Dan's great power generator

---
+++ 7 kyu/Dan's great power generator.js
@@ -0,0 +1,25 @@
+// Dan's great power generator
+
+/*
+Dan likes to find the nth power of number. But he doesn't like it when the result is odd!
+
+Return the nth power of each number, unless the result is odd, in which case round the result to the nearest 10.
+
+Eg. if number = 7, and power = 2, result = 50.
+FUNDAMENTALS
+ */
+
+function danspower(num, power) {
+  let res = Math.pow(num, power);
+  if (res % 2 === 0) { return res; }
+  else if (res % 10 >= 5) {
+    while (res % 10 !== 0) {
+      res++;
+    }
+  } else {
+    while (res % 10 !== 0) {
+      res--;
+    }
+  }
+  return res;
+}
