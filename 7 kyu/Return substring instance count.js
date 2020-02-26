717eb5f936ebc24abc91b2e6980bed2998039dd5
olenafrediuk <frediuk_olena@ukr.net>
2020-02-26 22:14:45 +0200

    Return substring instance count

---
+++ 7 kyu/Return substring instance count.js
@@ -0,0 +1,21 @@
+// Return substring instance count
+
+/*
+Complete the solution so that it returns the number of times the search_text is found within the full_text.
+
+Usage example:
+
+solution('aa_bb_cc_dd_bb_e', 'bb') # should return 2 since bb shows up twice
+solution('aaabbbcccc', 'bbb') # should return 1
+FUNDAMENTALS   STRINGS
+ */
+
+function solution(fullText, searchText) {
+  let pos = fullText.indexOf(searchText);
+  let count = 0;
+  while (pos !== -1) {
+    count ++;
+    pos = fullText.indexOf(searchText, pos + 1);
+  }
+  return count;
+}
