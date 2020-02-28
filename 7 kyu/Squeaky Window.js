118639b7325e1808d3043607eb4d1cc5012c817c
olenafrediuk <frediuk_olena@ukr.net>
2020-02-28 22:13:07 +0200

    Squeaky Window

---
+++ 7 kyu/Squeaky Window.js
@@ -0,0 +1,36 @@
+// Squeaky Window
+
+/*
+Write
+
+function sliding(nums, k)
+That given an array nums, there is a sliding window of size k which is moving from the very left of the array
+to the very right. You can only see the k numbers in the window.
+Each time the sliding window moves right by one position.
+
+For example:
+
+Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7      3
+1 [3  -1  -3] 5  3  6  7       3
+1  3 [-1  -3  5] 3  6  7       5
+1  3  -1 [-3  5  3] 6  7       5
+1  3  -1  -3 [5  3  6] 7       6
+1  3  -1  -3  5 [3  6  7]      7
+thus
+
+sliding([1,3,-1,-3,5,3,6,7], 3) === [3, 3, 5, 5, 6, 7]
+ALGORITHMS
+ */
+
+function sliding(nums, k) {
+  if (nums.length === 0) return [];
+  let maxArr = [];
+  for (let i = 0; i <= nums.length-k; i++) {
+    maxArr.push(Math.max(...nums.slice(i, i+k)));
+  }
+  return maxArr;
+}
