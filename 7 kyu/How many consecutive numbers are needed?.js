72617b9c9d6af9028a61bc2a74b32337efad854a
olenafrediuk <frediuk_olena@ukr.net>
2020-02-28 17:15:50 +0200

    How many consecutive numbers are needed?

---
+++ 7 kyu/How many consecutive numbers are needed?.js
@@ -0,0 +1,20 @@
+// How many consecutive numbers are needed?
+
+/*
+Create the function consecutive(arr) that takes an array of integers and return the minimum number of integers needed to make the contents of arr consecutive from the lowest number to the highest number.
+
+For example:
+If arr contains [4, 8, 6] then the output should be 2 because two numbers need to be added to the array (5 and 7) to make it a consecutive array of numbers from 4 to 8. Numbers in arr will be unique.
+FUNDAMENTALS   ARRAYS
+ */
+
+function consecutive(arr) {
+  let count = 0;
+  arr = arr.sort((a, b) => a - b);
+  if (arr.length < 2) return 0;
+  for (let i = 0; i < arr.length-1; i++) {
+    if  (Math.abs(arr[i+1] - arr[i]) > 1)
+      count +=Math.abs(arr[i+1] - arr[i])-1;
+  }
+  return count;
+}
