d7c8670fb736b74d6a658bb791702fc472694297
olenafrediuk <frediuk_olena@ukr.net>
2020-03-03 18:04:42 +0200

    Sentence to words

---
+++ 7 kyu/Sentence to words.js
@@ -0,0 +1,14 @@
+// Sentence to words
+
+/*
+Write function splitSentence which will create a list of strings from a string.
+
+Example:
+
+"hello world" -> ["hello", "world"]
+FUNDAMENTALS
+ */
+
+function splitSentence(s) {
+  return s.split(' ');
+}
