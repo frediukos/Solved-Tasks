00b4d195555ac93990086e8f8b5846aaffe1aaac
olenafrediuk <frediuk_olena@ukr.net>
2020-03-03 18:02:41 +0200

    Words to sentence

---
+++ 7 kyu/Words to sentence.js
@@ -0,0 +1,14 @@
+// Words to sentence
+
+/*
+Write function wordsToSentence which will create a string from a list of strings, separated by space.
+
+Example:
+
+["hello", "world"] -> "hello world"
+FUNDAMENTALS
+ */
+
+function wordsToSentence(words) {
+  return  words.join(' ');
+}
