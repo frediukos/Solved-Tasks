3725e08900bba0f84aaca8f33f95eb2eff5f4001
olenafrediuk <frediuk_olena@ukr.net>
2020-03-20 01:21:30 +0200

    TV channels

---
+++ 6 kyu/TV channels.js
@@ -0,0 +1,27 @@
+// TV channels
+
+/*
+Program channels into your TV's memory. An array with channels (strings) will be passed as an argument
+to the function redarr(). Sort the channels in an alphabetical order, remove duplicates and, finally,
+return an object where each channel (object's value) is assigned to a whole number (objects's key), starting with 0.
+
+Examples:
+
+var arr = ["BBC1", "BBC2", "MTV"];
+
+redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
+
+var arr = ["BBC1", "BBC1", "BBC2", "MTV"];
+
+redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
+
+FUNDAMENTALS   ARRAYS   OBJECTS
+ */
+
+function redarr(arr) {
+  const channel = {};
+  arr.filter((el, i) => arr.indexOf(el) === i)
+    .sort()
+    .map((el, i) => channel[i] = el);
+  return channel;
+}
