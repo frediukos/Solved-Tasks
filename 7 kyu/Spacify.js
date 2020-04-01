0c826eb39bfdb97efdd92720e6d87b7b7907d2e4
olenafrediuk <frediuk_olena@ukr.net>
2020-04-01 23:27:41 +0300

    Spacify

---
+++ 7 kyu/Spacify.js
@@ -0,0 +1,12 @@
+// Spacify
+
+/*
+Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+spacify("hello world") // returns "h e l l o   w o r l d"
+FUNDAMENTALS   STRINGS   ARRAYS
+ */
+
+function spacify(str) {
+  return str.split('').join(' ')
+}
