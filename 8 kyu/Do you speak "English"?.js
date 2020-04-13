7ea05a3c9b6925e23f4c74de02adb0e00d068cd5
olenafrediuk <frediuk_olena@ukr.net>
2020-04-13 23:52:04 +0300

    Do you speak "English"?

---
+++ 8 kyu/Do you speak "English"?.js
@@ -0,0 +1,18 @@
+// Do you speak "English"?
+
+/*
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+
+FUNDAMENTALS   LOOPS   CONTROL FLOW   BASIC LANGUAGE FEATURES
+ */
+
+function spEng(sentence){
+  let str = sentence.toLowerCase();
+  return str.includes('english');
+}
