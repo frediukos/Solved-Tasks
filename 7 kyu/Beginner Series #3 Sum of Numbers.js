ec43850a8421a618dc70114cf8ebb8ebbec5ad75
olenafrediuk <frediuk_olena@ukr.net>
2020-04-28 03:07:03 +0300

    Beginner Series #3 Sum of Numbers

--- 7 kyu/Beginner Series #3 Sum of Numbers.js
+++ 7 kyu/Beginner Series #3 Sum of Numbers.js
@@ -12,3 +12,27 @@ function getSum( a,b ) {
     }
     return sum;
 }
+
+// or
+
+function getSum(a, b) {
+    if (a === b) return a;
+    let arr = [a, b];
+    arr.sort((a, b) => a - b);
+    for (let i = arr[0]; i < arr[1]-1; i++) {
+        arr.push(i+1);
+    }
+    return arr.reduce((a, b) => a + b);
+}
+
+// or
+
+function getSum(a, b) {
+    let max = Math.max (a, b);
+    let min = Math.min (a, b);
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
+}
